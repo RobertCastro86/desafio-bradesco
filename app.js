36d842c245649d32d1f5b9f25d21a58d838b0447
Robert Castro <robertsmith86@hotmail.com>
2024-10-09 23:35:50 -0300

    adicionado melhoriass

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-'use strict';
+'use strict'
 
 const switcher = document.querySelector('.btn');
-switcher.addEventListener('click', function() {
-    document.body.classList.toggle('dark-theme');
 
-    const className = document.body.className;
-    if (className === 'light-theme') {
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+
+    var className = document.body.className;
+    if (className == 'light-theme') {
         this.textContent = 'Dark';
-    } else {
-        this.textContent = 'Light';
+    } 
+    /*console.log('current class name:' + className);*/
+    else {
+        this.textContent = 'light';
     }
 });
